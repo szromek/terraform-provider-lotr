167d70ce77d957c1174045c6fe82a9201ec1781a
Krzysztof Szromek <1729299+szromek@users.noreply.github.com>
2022-06-20 11:50:15 +0200

    Characters revisited

--- internal/provider/datasource_character.go
+++ internal/provider/datasource_character.go
@@ -3,8 +3,6 @@ package provider
 import (
 	"context"
 	"fmt"
-	"log"
-	"math/rand"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
@@ -17,66 +15,75 @@ var _ tfsdk.DataSource = characterDataSource{}
 
 type characterDataSourceType struct{}
 
+var characterSchema = map[string]tfsdk.Attribute{
+	"id": {
+		MarkdownDescription: "Character identifier",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"height": {
+		MarkdownDescription: "Height of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"race": {
+		MarkdownDescription: "race of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"gender": {
+		MarkdownDescription: "gender of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"birth": {
+		MarkdownDescription: "birth of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"spouse": {
+		MarkdownDescription: "spouse of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"death": {
+		MarkdownDescription: "death of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"realm": {
+		MarkdownDescription: "realm of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"hair": {
+		MarkdownDescription: "hair of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+	"name": {
+		MarkdownDescription: "name of a character",
+		Type:                types.StringType,
+		Computed:            true,
+		Optional:            false,
+	},
+	"wiki_url": {
+		MarkdownDescription: "wiki of a character",
+		Type:                types.StringType,
+		Computed:            true,
+	},
+}
+
 func (t characterDataSourceType) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
 	return tfsdk.Schema{
 		// This description is used by the documentation generator and the language server.
 		MarkdownDescription: "Character featuring in Lord of The Rings",
 
 		Attributes: map[string]tfsdk.Attribute{
-			"id": {
-				MarkdownDescription: "Character identifier",
-				Type:                types.StringType,
+			"characters": {
+				MarkdownDescription: "List of Lord of the Rings characters",
 				Computed:            true,
-			},
-			"height": {
-				MarkdownDescription: "Height of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"race": {
-				MarkdownDescription: "race of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"gender": {
-				MarkdownDescription: "gender of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"birth": {
-				MarkdownDescription: "birth of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"spouse": {
-				MarkdownDescription: "spouse of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"death": {
-				MarkdownDescription: "death of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"realm": {
-				MarkdownDescription: "realm of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"hair": {
-				MarkdownDescription: "hair of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"name": {
-				MarkdownDescription: "name of a character",
-				Type:                types.StringType,
-				Optional:            true,
-			},
-			"wiki_url": {
-				MarkdownDescription: "wiki of a character",
-				Type:                types.StringType,
-				Optional:            true,
+				Attributes:          tfsdk.ListNestedAttributes(characterSchema, tfsdk.ListNestedAttributesOptions{}),
 			},
 		},
 	}, nil
@@ -90,7 +97,7 @@ func (t characterDataSourceType) NewDataSource(ctx context.Context, in tfsdk.Pro
 	}, diags
 }
 
-type characterDataSourceData struct {
+type characterData struct {
 	Id      types.String `tfsdk:"id"`
 	Height  types.String `tfsdk:"height"`
 	Race    types.String `tfsdk:"race"`
@@ -104,23 +111,23 @@ type characterDataSourceData struct {
 	WikiUrl types.String `tfsdk:"wiki_url"`
 }
 
+type charactersListData struct {
+	CharacterData []characterData `tfsdk:"characters"`
+}
+
 type characterDataSource struct {
 	provider provider
 }
 
 func (d characterDataSource) Read(ctx context.Context, req tfsdk.ReadDataSourceRequest, resp *tfsdk.ReadDataSourceResponse) {
-	var data characterDataSourceData
+	var data charactersListData
 
 	diags := req.Config.Get(ctx, &data)
 	resp.Diagnostics.Append(diags...)
 
-	log.Printf("got here")
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
-
-	log.Printf("got here")
 
 	// If applicable, this is a great opportunity to initialize any necessary
 	// provider client data and make a call using it.
@@ -130,27 +137,28 @@ func (d characterDataSource) Read(ctx context.Context, req tfsdk.ReadDataSourceR
 		return
 	}
 
-	// characters[rand.Intn(len(characters))]
-
-	// For the purposes of this example code, hardcoding a response value to
-	// save into the Terraform state.
-	// data.Id = types.String{Value: "example-id"}
-
-	character := characters[rand.Intn(len(characters))]
-	characterState := characterDataSourceData{
-		Id:      types.String{Value: character.ID},
-		Height:  types.String{Value: character.Height},
-		Race:    types.String{Value: character.Race},
-		Gender:  types.String{Value: character.Gender},
-		Birth:   types.String{Value: character.Birth},
-		Spouse:  types.String{Value: character.Spouse},
-		Death:   types.String{Value: character.Death},
-		Realm:   types.String{Value: character.Realm},
-		Hair:    types.String{Value: character.Hair},
-		Name:    types.String{Value: character.Name},
-		WikiUrl: types.String{Value: character.WikiUrl},
+	var charactersFlattened []characterData
+	for _, character := range characters {
+		characterState := characterData{
+			Id:      types.String{Value: character.ID},
+			Height:  types.String{Value: character.Height},
+			Race:    types.String{Value: character.Race},
+			Gender:  types.String{Value: character.Gender},
+			Birth:   types.String{Value: character.Birth},
+			Spouse:  types.String{Value: character.Spouse},
+			Death:   types.String{Value: character.Death},
+			Realm:   types.String{Value: character.Realm},
+			Hair:    types.String{Value: character.Hair},
+			Name:    types.String{Value: character.Name},
+			WikiUrl: types.String{Value: character.WikiUrl},
+		}
+		charactersFlattened = append(charactersFlattened, characterState)
 	}
 
-	diags = resp.State.Set(ctx, characterState)
+	charactersDataList := charactersListData{
+		CharacterData: charactersFlattened,
+	}
+
+	diags = resp.State.Set(ctx, charactersDataList)
 	resp.Diagnostics.Append(diags...)
 }
